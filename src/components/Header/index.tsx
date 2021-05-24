aad0fc3e48d34c46ed2537ed481811b88f797d19
Igor Cunha <igorcesarcunha@gmail.com>
2021-05-24 08:17:38 -0400

    ignews

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,24 @@
+import styles from './styles.module.scss';
+import { SignInButton } from '../SignInButton';
+
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { ActiveLink } from '../ActiveLink';
+
+export function Header () {
+
+  const { asPath } = useRouter();
+
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <img src="/images/logo.svg" alt="ig.news" />
+        <nav>
+          <ActiveLink href="/" activeClassName={styles.active}><a>Home</a></ActiveLink>
+          <ActiveLink href="/posts" activeClassName={styles.active}><a>Posts</a></ActiveLink>
+        </nav>
+        <SignInButton />
+      </div>
+    </header>
+  );
+}
